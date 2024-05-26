9fb69d8f06a155f7d54c93880c94bf3ab2fe714d
Mouad <mouadlouhibi2000@gmail.com>
2024-05-26 19:50:19 +0100

    Class

--- app/src/main/java/com/louhibi/calculator/database/AppDatabase.kt
+++ app/src/main/java/com/louhibi/calculator/database/AppDatabase.kt
@@ -1,4 +1,34 @@
 package com.louhibi.calculator.database
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.louhibi.calculator.entities.Student
+
+@Database(entities = [Student::class], version = 1)
+abstract class AppDatabase : RoomDatabase(){ abstract fun studentDao(): StudentDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "app_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/louhibi/calculator/entities/Student.kt
+++ app/src/main/java/com/louhibi/calculator/entities/Student.kt
@@ -5,10 +5,9 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "student_table")
-data class Student {
-    @PrimaryKey(autoGenerate = true) val id: Int?,
-    @ColumnInfo(name = "first_name") val firstName: String?,
-    @ColumnInfo(name = "last_name") val lastName: String?,
-    @ColumnInfo(name = "roll_no") val rollNo: Int?
-    )
-}
+data class Student (
+    @PrimaryKey(autoGenerate = true) val id: Int? = null,
+    @ColumnInfo(name = "first_name") val firstName: String? = "",
+    @ColumnInfo(name = "last_name") val lastName: String? = "",
+    @ColumnInfo(name = "roll_no") val rollNo: Int? = null
+)
