7b74a4be14727ecc7e76c554307c8eafbdd9d2f6
Mouad <mouadlouhibi2000@gmail.com>
2024-05-27 11:08:06 +0100

    Class

--- app/src/main/java/com/louhibi/calculator/database/AppDatabase.kt
+++ app/src/main/java/com/louhibi/calculator/database/AppDatabase.kt
@@ -4,10 +4,10 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.louhibi.calculator.entities.Student
+import com.louhibi.calculator.entities.Todo
 
-@Database(entities = [Student::class], version = 1)
-abstract class AppDatabase : RoomDatabase(){ abstract fun studentDao(): StudentDao
+@Database(entities = [Todo::class], version = 1)
+abstract class AppDatabase : RoomDatabase(){ abstract fun todoDao(): TodoDao
 
     companion object {
         @Volatile
---
+++ app/src/main/java/com/louhibi/calculator/database/TodoDao.kt
@@ -0,0 +1,24 @@
+package com.louhibi.calculator.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.louhibi.calculator.entities.Todo
+
+@Dao
+interface TodoDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(todo: Todo)
+
+    @Delete
+    suspend fun delete(todo: Todo)
+
+    @Query("SELECT * from todo_table order by id ASC")
+    fun getAllTodos(): LiveData<List<Todo>>
+
+    @Query("UPDATE todo_table set title = :title, note = :note where id = :id")
+    suspend fun update(id: Int?, title: String?, note: String?)
+}
