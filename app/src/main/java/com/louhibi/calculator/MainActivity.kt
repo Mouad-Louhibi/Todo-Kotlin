3a89d6d8c0853ab15b9e0356742224559841b9d7
Mouad <mouadlouhibi2000@gmail.com>
2024-05-28 09:15:28 +0100

    MainActivity

--- app/src/main/java/com/louhibi/calculator/MainActivity.kt
+++ app/src/main/java/com/louhibi/calculator/MainActivity.kt
@@ -1,20 +1,88 @@
 package com.louhibi.calculator
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.louhibi.calculator.adaptors.TodoAdapter
+import com.louhibi.calculator.database.AppDatabase
+import com.louhibi.calculator.entities.Todo
+import com.louhibi.calculator.entities.TodoViewModel
+
+class MainActivity : AppCompatActivity(), TodoAdapter.TodoClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var database: AppDatabase
+    lateinit var viewModel: TodoViewModel
+    lateinit var adapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        ).get(TodoViewModel::class.java)
+
+        viewModel.allTodo.observe(this) { list ->
+            list?.let {
+                adapter.updateList(list)
+            }
         }
+
+        database = AppDatabase.getDatabase(this)
+    }
+
+    private fun initUI() {
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        adapter = TodoAdapter(this, this)
+        binding.recyclerView.adapter = adapter
+
+        val getContent =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val todo = result.data?.getSerializableExtra("todo") as? Todo
+                    if (todo != null) {
+                        viewModel.insertTodo(todo)
+                    }
+                }
+            }
+
+        binding.fabAddTodo.setOnClickListener {
+            val intent = Intent(this, AddTodoActivity::class.java)
+            getContent.launch(intent)
+        }
+
+    }
+
+    private val updateOrDeleteTodo =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val todo = result.data?.getSerializableExtra("todo") as Todo
+                val isDelete = result.data?.getBooleanExtra("delete_todo", false) as Boolean
+                if (todo != null && !isDelete) {
+                    viewModel.updateTodo(todo)
+                }else if(todo != null && isDelete){
+                    viewModel.deleteTodo(todo)
+                }
+            }
+        }
+
+
+
+    override fun onItemClicked(todo: Todo) {
+        val intent = Intent(this@MainActivity, AddTodoActivity::class.java)
+        intent.putExtra("current_todo", todo)
+        updateOrDeleteTodo.launch(intent)
     }
 }
